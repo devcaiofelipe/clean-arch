2233d87e3a2de14efa1c7e542c8cf0552f3fa656
Caio Felipe <devcaiofelipe15@gmail.com>
2023-09-15 05:56:54 -0300

    test: findAll method

--- src/shared/infrastructure/domain/repositories/tests/unit/in-memory-repository.spec.ts
+++ src/shared/infrastructure/domain/repositories/tests/unit/in-memory-repository.spec.ts
@@ -34,4 +34,11 @@ describe('InMemoryRepository unit tests', () => {
         const result = await sut.findById(entity.id);
         expect(entity.toJSON()).toStrictEqual(result.toJSON());
     });
+
+    it('should return all entitys', async () => {
+        const entity = new StubEntity({ name: 'test name', price: 50 });
+        await sut.insert(entity);
+        const result = await sut.findAll();
+        expect([entity]).toStrictEqual(result);
+    });
 });
