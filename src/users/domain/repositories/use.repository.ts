2ae7ef294b31134754bc7138252d8d8f897b0e8f
devcaiofelipe15@gmail.com <devcaiofelipe15@gmail.com>
2023-09-18 05:45:32 -0300

    feat: add SearchableRepositoryInterface

--- src/users/domain/repositories/use.repository.ts
+++ src/users/domain/repositories/use.repository.ts
@@ -1,7 +1,7 @@
-import { RepositoryInterface } from '@/shared/infrastructure/domain/repositories/repository-contracts';
 import { UserEntity } from '../entities/user.entity';
+import { SearchableRepositoryInterface } from '@/shared/infrastructure/domain/repositories/searchable-repository-contracts';
 
-export interface UserRepository extends RepositoryInterface<UserEntity> {
+export interface UserRepository extends SearchableRepositoryInterface<UserEntity, any, any> {
     findByEmail(email: string): Promise<UserEntity>;
     emailExists(email: string): Promise<void>;
 }
--- src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -1,10 +1,10 @@
 import { ConflictError } from "@/shared/infrastructure/domain/errors/conflict-error";
 import { NotFoundError } from "@/shared/infrastructure/domain/errors/not-found-error";
-import { InMemoryRepository } from "@/shared/infrastructure/domain/repositories/in-memory-repository";
+import { InMemorySearchableRepository } from "@/shared/infrastructure/domain/repositories/in-memory-repository-searchable";
 import { UserEntity } from "@/users/domain/entities/user.entity";
 import { UserRepository } from "@/users/domain/repositories/use.repository";
 
-export class UserInMemoryRepository extends InMemoryRepository<UserEntity> implements UserRepository {
+export class UserInMemoryRepository extends InMemorySearchableRepository<UserEntity> implements UserRepository {
     async findByEmail(email: string): Promise<UserEntity> {
         const entity = this.items.find((item) => item.email === email);
         if (!entity) {
