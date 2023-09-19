a7785fce54815ca551a4ae440bb5af952052f206
devcaiofelipe15@gmail.com <devcaiofelipe15@gmail.com>
2023-09-19 06:23:27 -0300

    feat: sort method

--- src/shared/infrastructure/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/infrastructure/domain/repositories/searchable-repository-contracts.ts
@@ -132,5 +132,6 @@ export interface SearchableRepositoryInterface<
     SearchInput = SearchParams,
     SearchOutput = SearchResult<E, Filter>,
     > extends RepositoryInterface<E> {
+    sortableFields: string[];
     search(props: SearchInput): Promise<SearchOutput>;
 }
