0db26ef29770f8d86fc944d8587b549ae15553f5
devcaiofelipe15@gmail.com <devcaiofelipe15@gmail.com>
2023-09-20 06:04:10 -0300

    feat: add method user in memory repository

--- src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -1,10 +1,12 @@
 import { ConflictError } from "@/shared/infrastructure/domain/errors/conflict-error";
 import { NotFoundError } from "@/shared/infrastructure/domain/errors/not-found-error";
 import { InMemorySearchableRepository } from "@/shared/infrastructure/domain/repositories/in-memory-repository-searchable";
+import { SortDirection } from "@/shared/infrastructure/domain/repositories/searchable-repository-contracts";
 import { UserEntity } from "@/users/domain/entities/user.entity";
 import { UserRepository } from "@/users/domain/repositories/use.repository";
 
-export class UserInMemoryRepository extends InMemorySearchableRepository<UserEntity> implements UserRepository {
+export class UserInMemoryRepository extends InMemorySearchableRepository<UserEntity> implements UserRepository.Repository {
+    sortableFields: string[] = ['name', 'createdAt'];
     async findByEmail(email: string): Promise<UserEntity> {
         const entity = this.items.find((item) => item.email === email);
         if (!entity) {
@@ -18,5 +20,16 @@ export class UserInMemoryRepository extends InMemorySearchableRepository<UserEnt
         if (entity) {
             throw new ConflictError('Email address already used email');
         }
+    }
+
+    protected async applyFilter(items: UserEntity[], filter: UserRepository.Filter): Promise<UserEntity[]> {
+        if (!filter) return items;
+        return items.filter(item => {
+            return item.props.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+        })
+    }
+
+    protected async applySort(items: UserEntity[], sort: string, sortDir: SortDirection | null): Promise<UserEntity[]> {
+        return !sort ? super.applySort(items, 'createdAt', 'desc') : super.applySort(items, sort, sortDir);
     }
 }
